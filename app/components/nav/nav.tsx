b7d811d28b3f9316ebf83fc37982e9c4faaa4b8b
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-14 22:24:04 +0300

    Basic Information Feed and Images

---
+++ app/components/nav/nav.tsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>About</li>
+                <li>Contact</li> 
+            </ul>      
+        </nav>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
-export default function Home () {
+import Nav from "./components/nav/nav";  
+
+export default function Home() {
   return (
-    <>
-    00 Home
-  01 Destination
-  02 Crew
-  03 Technology
-
-  So, you want to travel to
-  Space
-  Let’s face it; if you want to go to space, you might as well genuinely go to 
-  outer space and not hover kind of on the edge of it. Well sit back, and relax 
-  because we’ll give you a truly out of this world experience!
-
-  Explore
-    </>
-  )
+    <section>
+      <Nav/>
+      <div>
+        <p>So, you want to travel to</p>
+        <h1>Space</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <a href="#">
+        <button>Explore</button>
+      </a>
+    </section>
+  );
 }
