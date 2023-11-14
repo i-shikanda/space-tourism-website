8e20dd1bf88fb0505e95f3f701b6fe36a4f257ce
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-15 00:08:03 +0300

    Nav and Homepage Styling

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,12 +1,38 @@
+import Image from "next/image";
+
 export default function Nav() {
-    return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Destinations</li>
-                <li>About</li>
-                <li>Contact</li> 
-            </ul>      
-        </nav>
-    )
+  return (
+    <nav className="flex justify-between items-center py-8">
+      <div className="px-20">
+        <Image
+          src="/assets/shared/logo.svg"
+          alt="Space-Logo"
+          width={60}
+          height={60}
+        />
+      </div>
+      <ul className="flex bg-gray-700 bg-opacity-30 backdrop-blur-md items-center px-20 uppercase space-x-8">
+        <a href="" className="h-full border-b-2 py-8">
+          <li >
+            <span className="font-bold">00 </span>Home
+          </li>
+        </a>
+        <a href="" className="h-full active:border-b-2 py-8">
+          <li>
+            <span className="font-bold">01 </span>Destinations
+          </li>
+        </a>
+        <a href="" className="h-full active:border-b-2 py-8">
+          <li>
+            <span className="font-bold">02 </span>About
+          </li>
+        </a>
+        <a href="" className="h-full active:border-b-2 py-8">
+          <li>
+            <span className="font-bold">03 </span>Contact
+          </li>
+        </a>
+      </ul>
+    </nav>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,28 @@
-import Nav from "./components/nav/nav";  
+import Nav from "./components/nav/nav";
 
 export default function Home() {
   return (
-    <section>
-      <Nav/>
-      <div>
-        <p>So, you want to travel to</p>
-        <h1>Space</h1>
-        <p>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
+    <section className="h-[100vh]">
+      <header className="w-full">
+        <Nav />
+      </header>
+      <div className="flex justify-between items-end h-[70vh] px-24 ">
+        <div>
+          <h6 className="uppercase text-xl">So, you want to travel to</h6>
+          <h1 className="font-black text-8xl uppercase py-8 px-2">Space</h1>
+          <p className="text-lg">
+            Let’s face it; if you want to go to space, you might as well <br />
+            genuinely go to outer space and not hover kind of on the <br /> edge
+            of it. Well sit back, and relax because we’ll give you <br /> a
+            truly out of this world experience!
+          </p>
+        </div>
+        <a href="#">
+          <button className="bg-white rounded-full px-20 py-28 text-black uppercase">
+            Explore
+          </button>
+        </a>
       </div>
-      <a href="#">
-        <button>Explore</button>
-      </a>
     </section>
   );
 }
